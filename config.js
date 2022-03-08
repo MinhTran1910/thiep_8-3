92e609f48a611916db8d9a1a0b88f0c613635391
MinhTran1910 <96324134+MinhTran1910@users.noreply.github.com>
2022-03-08 11:44:18 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = 'Tuyền và Hiền';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'friend';
+const contentCard = "Ngày 8/3 đã đến rồi, chúc một nửa xinh đẹp của thế giới luôn thành công và hạnh phúc trong cuộc sống";
+
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'tuyenhien.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
